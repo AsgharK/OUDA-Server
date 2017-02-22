a7c89a241048c107436d551e890d6163a7b862d4
Asghar Khan <asgharakhan94@gmail.com>
2017-02-22 15:36:56 -0500

    Get team name from request body.

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 router.post('/notifyReferee', function(req,res){
 	var socket = req.app.get('socketIo');
-	socket.emit('notify', 'Offside has occured.');
-	res.send("Notification Sent");
+	message = "Team " + req.body.team + " offside!";
+	socket.emit('notify', message);
+	res.send({ "status": "Notification Sent" });
 });
 
 module.exports = router;
